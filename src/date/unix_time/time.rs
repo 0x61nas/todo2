12463404eb91dd50ac0ca8643b079bd0befaac27
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2023-09-21 01:14:06 +0300

    feat(time): implement the time back-end

--- src/date/unix_time/time.rs
+++ src/date/unix_time/time.rs
@@ -1,6 +1,21 @@
-use crate::date::unix_time::UnixTimeCalc;
-use time::{Date, PrimitiveDateTime, Time};
+use time::{Date, Month, PrimitiveDateTime, Time};
 
-pub(crate) struct TimeCalc;
+pub(crate) struct TimeCalc {
+    pub(super) year: i32,
+    pub(super) month: Option<u8>,
+    pub(super) day: Option<u8>,
+}
 
-impl UnixTimeCalc for TimeCalc {}
+impl TimeCalc {
+    #[inline(always)]
+    pub(super) fn _calc(&self) -> Result<u64, String> {
+        let Ok(dt) = Date::from_calendar_date(self.year,
+                                                Month::try_from(
+                                                    self.month.unwrap_or(1)).unwrap(),
+                                                self.day.unwrap_or(1)) else {
+            return Err("Invalid date".to_string());
+        };
+        let dt = PrimitiveDateTime::new(dt, Time::from_hms(0, 0, 0).expect("Internal error"));
+        Ok(dt.assume_utc().unix_timestamp() as u64)
+    }
+}
