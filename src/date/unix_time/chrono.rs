f2f9a0a0e03f6680da027809b384d9651629196a
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2023-09-20 13:50:32 +0300

    clean(unixtime): remove the boilerplate code

--- src/date/unix_time/chrono.rs
+++ src/date/unix_time/chrono.rs
@@ -1,50 +1,14 @@
-use crate::date::unix_time::UnixTimeCalc;
 use chrono::NaiveDate;
 
 pub(crate) struct ChronoCalc {
-    year: i32,
-    month: Option<u32>,
-    day: Option<u32>,
+    pub(crate) year: i32,
+   pub(crate) month: Option<u32>,
+    pub(super) day: Option<u32>,
 }
 
-impl UnixTimeCalc for ChronoCalc {
-    fn new() -> Self {
-        Self {
-            year: 0,
-            month: None,
-            day: None,
-        }
-    }
-
-    fn year(&mut self, year: String) -> Result<(), String> {
-        self.year = year.parse::<i32>().map_err(|_| "Invalid year")?;
-        Ok(())
-    }
-
-    fn month(&mut self, month: String) -> Result<(), String> {
-        self.month = Some(month.parse::<u32>().map_err(|_| "Invalid month")?);
-        Ok(())
-    }
-
-    fn day(&mut self, day: String) -> Result<(), String> {
-        self.day = Some(day.parse::<u32>().map_err(|_| "Invalid day")?);
-        Ok(())
-    }
-
-    fn is_year_set(&self) -> bool {
-        self.year != 0
-    }
-
-    fn is_month_set(&self) -> bool {
-        self.month.is_some()
-    }
-
-    fn is_day_set(&self) -> bool {
-        self.day.is_some()
-    }
-
-
-    fn calc(&self) -> Result<u64, String> {
+impl ChronoCalc {
+    #[inline(always)]
+    pub(super) fn _calc(&self) -> Result<u64, String> {
         let date = NaiveDate::from_ymd_opt(self.year, self.month.unwrap_or(1), self.day.unwrap_or(1))
             .ok_or_else(|| "Invalid date".to_string())?;
         Ok(date.and_hms_opt(0, 0, 0).expect("Internal error").timestamp() as u64)
--- src/date/unix_time/mod.rs
+++ src/date/unix_time/mod.rs
@@ -20,3 +20,53 @@ pub(super) trait UnixTimeCalc {
     fn is_day_set(&self) -> bool;
     fn calc(&self) -> Result<u64, String>;
 }
+
+macro_rules! impl_unixtime_calc {
+    ($t:ty, $mt:ty, $dt:ty) => {
+            impl UnixTimeCalc for $t {
+                fn new() -> Self {
+                    Self {
+                        year: 0,
+                        month: None,
+                        day: None,
+                    }
+                }
+
+                fn year(&mut self, year: String) -> Result<(), String> {
+                    self.year = year.parse::<i32>().map_err(|_| "Invalid year")?;
+                    Ok(())
+                }
+
+                fn month(&mut self, month: String) -> Result<(), String> {
+                    self.month = Some(month.parse::<$mt>().map_err(|_| "Invalid month")?);
+                    Ok(())
+                }
+
+                fn day(&mut self, day: String) -> Result<(), String> {
+                    self.day = Some(day.parse::<$dt>().map_err(|_| "Invalid day")?);
+                    Ok(())
+                }
+
+                fn is_year_set(&self) -> bool {
+                    self.year != 0
+                }
+
+                fn is_month_set(&self) -> bool {
+                    self.month.is_some()
+                }
+
+                fn is_day_set(&self) -> bool {
+                    self.day.is_some()
+                }
+
+                fn calc(&self) -> Result<u64, String> {
+                    self._calc()
+                }
+            }
+    };
+}
+
+#[cfg(feature = "chrono-backend")]
+impl_unixtime_calc!(chrono::ChronoCalc, u32, u32);
+#[cfg(feature = "time-backend")]
+impl_unixtime_calc!(time::TimeCalc, u8, u8);
