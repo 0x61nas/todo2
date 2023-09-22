95a5564a9a290f019b3b5bdfd79ae42c8da209a3
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2023-09-22 04:05:30 +0300

    test(unixtime): create unit tests for the time back-ends

--- src/date/unix_time/mod.rs
+++ src/date/unix_time/mod.rs
@@ -72,3 +72,70 @@ macro_rules! impl_unixtime_calc {
 impl_unixtime_calc!(chrono::ChronoCalc, u32, u32);
 #[cfg(feature = "time-backend")]
 impl_unixtime_calc!(time::TimeCalc, u8, u8);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const YYY_MM_DD_INPUTS: [((&str, &str, &str), u64); 3] = [
+        (("2003", "2", "2"), 1_044_144_000),
+        (("2001", "2", "28"), 983_318_400),
+        (("2033", "3", "26"), 1_995_408_000),
+    ];
+
+    const YYY_MM_INPUTS: [((&str, &str), u64); 3] = [
+        (("2003", "2"), 1_044_057_600),
+        (("2001", "2"), 980_985_600),
+        (("2033", "3"), 1_993_248_000),
+    ];
+
+    const YYY_INPUTS: [(&str, u64); 3] = [
+        ("2003", 1_041_379_200),
+        ("2001", 978_307_200),
+        ("2033", 1_988_150_400),
+    ];
+
+    fn test_calc<C: UnixTimeCalc>() -> Result<()> {
+        for ((year, month, day), expected) in YYY_MM_DD_INPUTS.iter() {
+            let mut calc = C::new();
+            calc.year(year.to_string())?;
+            calc.month(month.to_string())?;
+            calc.day(day.to_string())?;
+            assert_eq!(calc.calc()?, *expected);
+        }
+
+        for ((year, month), expected) in YYY_MM_INPUTS.iter() {
+            let mut calc = C::new();
+            calc.year(year.to_string())?;
+            calc.month(month.to_string())?;
+            assert_eq!(calc.calc()?, *expected);
+        }
+
+        for (year, expected) in YYY_INPUTS.iter() {
+            let mut calc = C::new();
+            calc.year(year.to_string())?;
+            assert_eq!(calc.calc()?, *expected);
+        }
+
+        Ok(())
+    }
+
+   #[cfg(not(any(feature = "chrono-backend", feature = "time-backend")))]
+    #[test]
+   #[ignore] // We know that the simple backend is not accurate right now
+    fn test_calc_simple() -> Result<()> {
+        test_calc::<simple::SimpleCalc>()
+    }
+
+    #[cfg(feature = "chrono-backend")]
+    #[test]
+    fn test_calc_chrono() -> Result<()> {
+        test_calc::<chrono::ChronoCalc>()
+    }
+
+    #[cfg(feature = "time-backend")]
+    #[test]
+    fn test_calc_time() -> Result<()> {
+        test_calc::<time::TimeCalc>()
+    }
+}
